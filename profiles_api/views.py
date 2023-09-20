cfcff6e404a0326c7ae5b7923a8b15f6fb748175
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-20 10:33:30 -0700

    d

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -99,3 +100,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFileter,)
+    search_fields = ('name', 'email',)
